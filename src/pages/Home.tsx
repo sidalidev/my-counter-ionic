8651aa36088ac829e0bb466f10ce5dbeefc6433a
Sid Bentifraouine <sidbentifraouine@gmail.com>
2020-01-14 09:47:12 +0100

    feat(simple-counter): non below zero counter

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,33 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+  IonText
+} from "@ionic/react";
+import React, { useState } from "react";
 
 const Home: React.FC = () => {
+  const [count, setCount] = useState(0);
+
+  function incrementCounter() {
+    setCount(count + 1);
+  }
+
+  function decrementCounter() {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  }
+
+  function renderBelowZeroAlert() {
+    if (count < 1) {
+      return <IonText style={{ color: "red" }}>Can't go below 0</IonText>;
+    }
+  }
+
   return (
     <IonPage>
       <IonHeader>
@@ -10,17 +36,39 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
+        <div style={styles.container}>
+          <IonText style={styles.title}>Counter App</IonText>
+          <div style={styles.countContainer}>
+            <IonText>{count}</IonText>
+            <div style={styles.actionButtons}>
+              <IonButton onClick={decrementCounter}>-</IonButton>
+              <IonButton onClick={incrementCounter}>+</IonButton>
+            </div>
+            {renderBelowZeroAlert()}
+          </div>
+        </div>
       </IonContent>
     </IonPage>
   );
 };
 
 export default Home;
+
+const styles = {
+  container: {
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  countContainer: {
+    paddingTop: 100,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  title: {
+    fontSize: 20
+  },
+  actionButtons: {
+    justifyContent: "center",
+    alignItems: "center"
+  }
+};
